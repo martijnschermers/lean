7b7729849b183d2ca0cb58f1a1489038b1f97e3b
Martijn <martijnschermers2@gmail.com>
2022-11-13 18:52:59 +0100

    Add: HttpException filter

---
+++ apps/api/src/filters/http.filter.ts
@@ -0,0 +1,21 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    return response
+      // @ts-ignore
+      .status(status)
+      .json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        message: exception.message,
+        path: request.url,
+      });
+  }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,11 +7,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { HttpExceptionFilter } from "./filters/http.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalFilters(new HttpExceptionFilter());
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
