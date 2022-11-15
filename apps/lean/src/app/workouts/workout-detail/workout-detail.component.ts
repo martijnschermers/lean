2f48184f3da2cce1f9eed610eb656b738cfe2f59
martijnschermers <martijnschermers2@gmail.com>
2022-11-15 15:38:09 +0100

    Add: workout detail component

---
+++ apps/lean/src/app/workouts/workout-detail/workout-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lean-workout-detail',
+  templateUrl: './workout-detail.component.html',
+  styleUrls: ['./workout-detail.component.css'],
+})
+export class WorkoutDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
