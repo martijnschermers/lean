577df4ab7588943a6d6075aec1d40d28100163e2
martijnschermers <martijnschermers2@gmail.com>
2022-11-15 15:39:23 +0100

    Add: workout component

---
+++ apps/lean/src/app/workouts/workout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lean-workouts',
+  templateUrl: './workout.component.html',
+  styleUrls: ['./workout.component.css'],
+})
+export class WorkoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
