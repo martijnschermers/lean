69a738a8825ed79dfdafe39d7c020b636cc8e434
Martijn <martijnschermers2@gmail.com>
2022-11-20 14:01:54 +0100

    Update: extracted navbar to component

--- apps/lean/src/app/app.component.ts
+++ apps/lean/src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'lean-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "lean-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
 }
