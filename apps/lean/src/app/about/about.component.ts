8159511ef29bd73163e524f49d8d17510039c64d
Martijn <martijnschermers2@gmail.com>
2022-11-20 14:11:47 +0100

    Update: small code/style cleanups

--- apps/lean/src/app/about/about.component.ts
+++ apps/lean/src/app/about/about.component.ts
@@ -7,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   userStories = [
-    'Als gebuiker wil ik een workout kunnen starten, zodat ik mijn uitgevoerde oefeningen bij kan houden.',
+    'Als gebruiker wil ik een workout kunnen starten, zodat ik mijn uitgevoerde oefeningen bij kan houden.',
     'Als gebruiker wil ik sets kunnen toevoegen aan een workout, zodat ik mijn uitgevoerde sets bij kan houden.',
+    'Als gebruiker wil ik sets kunnen verwijderen uit een workout, zodat ik sets kan verwijderen die ik niet heb uitgevoerd.',
     'Als gebruiker wil ik een zelf aangemaakte oefening toe kunnen voegen aan een workout, zodat ik mijn eigen oefeningen kan gebruiken.',
     'Als gebruiker wil ik een workout kunnen aanpassen, zodat ik eventuele fouten kan corrigeren.',
     'Als gebruiker wil ik een workout kunnen verwijderen, zodat ik ongewenste workouts kan verwijderen.',
     'Als gebruiker wil ik een workout kunnen bekijken, zodat ik mijn uitgevoerde oefeningen kan bekijken.',
+    'Als gebruiker wil ik een andere gebruiker kunnen volgen, zodat ik zijn workouts kan bekijken.',
+    'Als gebruiker wil ik deel kunnen nemen aan een group workout, zodat ik samen met anderen kan trainen.',
+    'Als gebruiker wil ik een custom oefening toe kunnen voegen, zodat ik niet afhankelijk ben van de oefeningen die al bestaan.',
+    'Als gebruiker wil ik een custom oefening kunnen aanpassen, zodat ik eventuele fouten kan corrigeren.',
+    'Als gebruiker wil ik een custom oefening kunnen verwijderen, zodat ik ongewenste oefeningen kan verwijderen.',
+    'Als gebruiker wil ik een account aan kunnen maken, zodat mijn workouts en oefeningen bewaard blijven.',
+    'Als gebruiker wil ik in kunnen loggen met mijn account, zodat ik mijn workouts en oefeningen kan bekijken.',
   ]
 
   constructor() {}
--- apps/lean/src/app/user/user-detail/user-detail.component.ts
+++ apps/lean/src/app/user/user-detail/user-detail.component.ts
@@ -1,33 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { User } from '@lean/api-interfaces';
-import { UserService } from '../user.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { User } from "@lean/api-interfaces";
+import { UserService } from "../user.service";
 import { Location } from "@angular/common";
 
 @Component({
-  selector: 'lean-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css'],
+  selector: "lean-user-detail",
+  templateUrl: "./user-detail.component.html",
+  styleUrls: ["./user-detail.component.css"]
 })
 export class UserDetailComponent implements OnInit {
   @Input()
-  user: User; 
+  user: User;
 
-  constructor(private route: ActivatedRoute, private userService: UserService, private location: Location) {}
+  constructor(private route: ActivatedRoute, private userService: UserService, private location: Location) {
+  }
 
   ngOnInit(): void {
     this.getUser();
   }
 
   getUser(): void {
-    const id = this.route.snapshot.paramMap.get('id');
+    const id = this.route.snapshot.paramMap.get("id");
 
     if (id != null) {
       this.userService.getUser(id).subscribe((user) => (this.user = user));
-      return; 
+      return;
     }
 
-    return; 
+    return;
   }
 
   updateUser(): void {
