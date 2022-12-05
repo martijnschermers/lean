dc86b492339ecf34afaef9ced0b9d329ce90550c
Martijn <martijnschermers2@gmail.com>
2022-12-05 11:32:32 +0100

    Add: login && logout link

--- apps/lean/src/app/components/navbar/navbar.component.ts
+++ apps/lean/src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../../auth/auth.service";
-import { Observable } from "rxjs";
-import { UserInterface } from "@lean/api-interfaces";
+import { BehaviorSubject } from "rxjs";
+import { IdentityInterface } from "@lean/api-interfaces";
 
 @Component({
-  selector: 'lean-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
+  selector: "lean-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
-  loggedInUser$: Observable<UserInterface>
+  loggedInUser$: BehaviorSubject<IdentityInterface | undefined> = new BehaviorSubject<IdentityInterface | undefined>(undefined);
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
-    // this.loggedInUser$ = this.authService.currentUser$;
+    this.loggedInUser$ = this.authService.currentUser$;
   }
 
   logout(): void {
