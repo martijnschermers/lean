8f1b8a3b6497c07606c959df3d5455dc031e2a25
Martijn Schermers <martijnschermers2@gmail.com>
2026-02-11 21:58:01 +0100

    migrated common components to v20

--- apps/lean/src/app/components/icon/icon.component.ts
+++ apps/lean/src/app/components/icon/icon.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "lean-icon",
   templateUrl: "./icon.component.html",
-  styleUrls: ["./icon.component.css"]
+  styleUrls: ["./icon.component.css"],
+  imports: [CommonModule]
 })
 export class IconComponent {
   @Input()
--- apps/lean/src/app/home/home.component.ts
+++ apps/lean/src/app/home/home.component.ts
@@ -3,11 +3,15 @@ import { AuthService } from "../auth/auth.service";
 import { WorkoutService } from "../workout/workout.service";
 import { Observable } from "rxjs";
 import { WorkoutInterface } from "@lean/api-interfaces";
+import { CommonModule } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { WorkoutCardComponent } from "../workout/workout-card/workout-card.component";
 
 @Component({
   selector: "lean-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  styleUrls: ["./home.component.css"],
+  imports: [CommonModule, RouterLink, WorkoutCardComponent]
 })
 export class HomeComponent {
   currentUser$ = this.authService.currentUser$.value;
