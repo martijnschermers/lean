088533f63c2f996da4af4589866ba1c0dbb61320
Martijn Schermers <martijnschermers2@gmail.com>
2026-02-11 21:57:37 +0100

    migrated auth components to v20

--- apps/lean/src/app/auth/login/login.component.ts
+++ apps/lean/src/app/auth/login/login.component.ts
@@ -4,11 +4,15 @@ import { AuthService } from "../auth.service";
 import { Router } from "@angular/router";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { catchError, map } from "rxjs";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterLinkWithHref } from "@angular/router";
 
 @Component({
   selector: "lean-login",
   templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+  styleUrls: ["./login.component.css"],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLinkWithHref]
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
